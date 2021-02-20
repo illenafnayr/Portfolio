57fbef4d39668b8df09069424aa611dfe189ca70
Ryan Fanelli <illenafnayr@gmail.com>
2021-02-19 22:25:35 -0500

    display current time updates every 60 seconds

--- src/App.vue
+++ src/App.vue
@@ -9,25 +9,7 @@
 <style>
 body {
   margin: 0;
-}
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  background-color: rgb(3,128,131)
 }
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -4,6 +4,9 @@
         <img src="../assets/windows.png" alt="windows" id="windowslogo" >
         <span>START</span>
     </div>
+    <div id="clock">
+        <span>{{ timestamp }}</span>
+    </div>
 </div>
 
 </template>
@@ -13,6 +16,26 @@ export default {
   name: 'NavBar',
   props: {
     msg: String
+  },
+  data() {
+      return {
+          timestamp: ''
+    }
+  },
+  mounted() {
+      this.getTime()
+  },
+  created() {
+      setInterval(this.getTime, (1000*60))
+  },
+  methods: {
+      getTime: function() {
+          const today = new Date();
+        //   const date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+          this.timestamp = today.getHours() + ":" + today.getMinutes();
+        //   const time = time;
+        //   this.timestamp = time;
+      }
   }
 }
 </script>
@@ -50,6 +73,23 @@ export default {
 
 #startbtn:active {
     border-color: #808080 #FFFFFF #FFFFFF #808080 ;
+}
+
+#clock {
+    font-family: 'VT323', monospace;
+    height: 80%;
+    right: 3px;
+    border: .5px solid black;
+    box-sizing: border-box;
+    margin-top: 2px;
+    margin-bottom: 2px;
+    position: absolute;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 1px 2px 1px 2px;
+    border-width:1px;
+    border-color: #808080 #FFFFFF #FFFFFF #808080 
 }
 
 #windowslogo {
