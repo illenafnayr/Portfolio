34855b3452cad271ce6fce3df26f16f44f916d5f
Ryan Fanelli <illenafnayr@gmail.com>
2021-02-22 20:44:18 -0500

    startmenu has sidebar with title, blocks are styled but dont have content

--- src/components/Desktop.vue
+++ src/components/Desktop.vue
@@ -1,6 +1,6 @@
 <template>
 <div id="navbar">
-    <StartMenu v-bind:style='{"display": (isActive? "flex" : "none")}' />
+    <StartMenu v-bind:style='{"display": (isActive? "block" : "none")}' />
     <div id="btnContainer">
         <div id="startbtn" v-on:click="showMenu()" v-bind:style='{"border-color": (isActive? "#808080 #FFFFFF #FFFFFF #808080" : "#FFFFFF #808080 #808080 #FFFFFF")}' >
             <img src="../assets/windows.png" alt="windows" id="windowslogo" >
@@ -62,69 +62,4 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 @import url('https://fonts.googleapis.com/css2?family=VT323&display=swap');
-
-
-#navbar {
-    position: absolute;
-    bottom: 0%;
-    height: 35px;
-    width: 100vw;
-    background-color: rgb(192,192,192);
-    border-top: 1px solid black;
-    border-width:2px;
-    border-color: #ffffff;
-    display: flex;
-}
-
-#btnContainer {
-    height: 100%;
-    display: flex;
-    align-items: center;
-}
-
-#startbtn {
-    font-family: 'VT323', monospace;
-    height: 80%;
-    left: 5px;
-    border: .5px solid black;
-    box-sizing: border-box;
-    position: absolute;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    padding: 1px 2px 1px 2px;
-    border-width:1px;
-    border-color:#FFFFFF #808080 #808080 #FFFFFF;
-    cursor: pointer;
-}
-
-/* #startbtn:active {
-    border-color: #808080 #FFFFFF #FFFFFF #808080;
-} */
-
-#clockContainer {
-    height: 100%;
-    display: flex;
-    align-items: center;
-}
-
-#clock {
-    font-family: 'VT323', monospace;
-    height: 80%;
-    right: 5px;
-    border: .5px solid black;
-    box-sizing: border-box;
-    position: absolute;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    padding: 1px 2px 1px 2px;
-    border-width:1px;
-    border-color: #808080 #FFFFFF #FFFFFF #808080 
-}
-
-#windowslogo {
-    height: 98%;
-}
-
-</style>
+/* font-family: 'VT323', monospace; */
--- src/components/StartMenu.vue
+++ src/components/StartMenu.vue
@@ -1,6 +1,34 @@
 <template>
 <div id="startMenu">
 
+  <div id="sidebar">
+    <span id="startMenuTitle">Ryan Fanelli Software Engineer</span>
+  </div>
+
+  <div id="appList">
+      <div class="block" >
+        Portfolio
+      </div>
+      <div class="block" >
+        Portfolio
+      </div>
+      <div class="block" >
+        Portfolio
+      </div>
+      <div class="block" >
+        Portfolio
+      </div>
+      <div class="block" >
+        Portfolio
+      </div>
+      <div class="block" >
+        Portfolio
+      </div>
+      <hr>
+      <div class="block" >
+        Portfolio
+      </div>
+  </div>
 </div>
 </template>
 
@@ -18,11 +46,59 @@ export default {
     position: absolute;
     left: 1.5px;
     bottom: 38px;
-    width: 120px;
+    width: 175px;
     height: 275px;
-    background-color: black;
-    border: 2px solid red;
-    display: none;
+    background-color: rgb(191,191,191);
+    border: 1px solid black;
+    /* border-width:1px; */
+    border-color:#FFFFFF #808080 #808080 #FFFFFF;
+    display: flex;
+}
+
+#sidebar{
+  height: 100%;
+  width: 12.5%;
+  background-color: rgb(0,0,123);
+  display: flex;
+  justify-content: center;
+  align-items: flex-end;
+}
+
+#appList {
+  height: 100%;
+  width: 90%;
+  /* border: 1px solid gold; */
+  display: flex;
+  flex-direction: column;
+}
+
+#startMenuTitle{
+  writing-mode:vertical-lr;
+  transform: rotate(180deg);
+  color: #FFFFFF;
+  margin-bottom: 40%;
+}
+
+hr {
+  width: "98%";
+  margin: 0;
+}
+
+.block {
+  height: 14.2857142857%;
+  box-sizing: border-box;
+  /* border-bottom: 1px solid black;
+  border-right: 1px solid black; */
+  width: 100%;
+  /* background-color: aqua; */
+  display: flex;
+  align-items: center;
+}
+
+.block:hover {
+  cursor: pointer;
+  background-color: rgb(0,0,123);
+  color: #FFFFFF;
 }
 
 </style>
