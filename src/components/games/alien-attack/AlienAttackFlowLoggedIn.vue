be100243123e3d6af463727233d256eedfe11afc
Ryan Fanelli <illenafnayr@gmail.com>
2021-07-18 19:48:58 -0400

    character moves on wads keys

--- src/components/games/alien-attack/AlienAttackFlowLoggedIn.vue
+++ src/components/games/alien-attack/AlienAttackFlowLoggedIn.vue
@@ -84,52 +84,121 @@ export default {
     //   }
     // },
     drawCanvas() {
-      let img = new Image();
-      img.src = "https://freepikpsd.com/media/2020/03/alien-sprite-png-1.png";
-      img.onload = function () {
-        init();
-      };
+      const SCALE = .5;
+      const WIDTH = this.SPRITE_WIDTH;
+      const HEIGHT = this.SPRITE_HEIGHT;
+      const SCALED_WIDTH = SCALE * WIDTH;
+      const SCALED_HEIGHT = SCALE * HEIGHT;
+      const CYCLE_LOOP = [0, 1, 0, 2];
+      const FACING_DOWN = 0;
+      const FACING_UP = 3;
+      const FACING_LEFT = 1;
+      const FACING_RIGHT = 2;
+      const FRAME_LIMIT = 12;
+      const MOVEMENT_SPEED = 1;
 
       let canvas = document.querySelector("canvas");
       let ctx = canvas.getContext("2d");
-      const width = this.SPRITE_WIDTH;
-      const height = this.SPRITE_HEIGHT;
-      const scaledWidth = width * 0.75;
-      const scaledHeight = height * 0.75;
+      let keyPresses = {};
+      let currentDirection = FACING_DOWN;
+      let currentLoopIndex = 0;
+      let frameCount = 0;
+      let positionX = 0;
+      let positionY = 0;
+      let img = new Image();
+
+      window.addEventListener("keydown", keyDownListener);
+      function keyDownListener(event) {
+        keyPresses[event.key] = true;
+      }
+
+      window.addEventListener("keyup", keyUpListener);
+      function keyUpListener(event) {
+        keyPresses[event.key] = false;
+      }
+
+      function loadImage() {
+        img.src = "https://freepikpsd.com/media/2020/03/alien-sprite-png-1.png";
+        img.onload = function () {
+          window.requestAnimationFrame(gameLoop);
+        };
+      }
+
       function drawFrame(frameX, frameY, canvasX, canvasY) {
         ctx.drawImage(
           img,
-          frameX * width,
-          frameY * height,
-          width,
-          height,
+          frameX * WIDTH,
+          frameY * HEIGHT,
+          WIDTH,
+          HEIGHT,
           canvasX,
           canvasY,
-          scaledWidth,
-          scaledHeight
+          SCALED_WIDTH,
+          SCALED_HEIGHT
         );
       }
-      const cycleLoop = [0, 1, 2, 1];
-      let currentLoopIndex = 0;
-      let frameCount = 0;
 
-      function step() {
-        frameCount++;
-        if (frameCount < 15) {
-          window.requestAnimationFrame(step);
-          return;
-        }
-        frameCount = 0;
+      loadImage();
+
+      function gameLoop() {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
-        drawFrame(cycleLoop[currentLoopIndex], 0, 0, 0);
-        currentLoopIndex++;
-        if (currentLoopIndex >= cycleLoop.length) {
+
+        let hasMoved = false;
+
+        if (keyPresses.w) {
+          moveCharacter(0, -MOVEMENT_SPEED, FACING_UP);
+          hasMoved = true;
+        } else if (keyPresses.s) {
+          moveCharacter(0, MOVEMENT_SPEED, FACING_DOWN);
+          hasMoved = true;
+        }
+
+        if (keyPresses.a) {
+          moveCharacter(-MOVEMENT_SPEED, 0, FACING_LEFT);
+          hasMoved = true;
+        } else if (keyPresses.d) {
+          moveCharacter(MOVEMENT_SPEED, 0, FACING_RIGHT);
+          hasMoved = true;
+        }
+
+        if (hasMoved) {
+          frameCount++;
+          if (frameCount >= FRAME_LIMIT) {
+            frameCount = 0;
+            currentLoopIndex++;
+            if (currentLoopIndex >= CYCLE_LOOP.length) {
+              currentLoopIndex = 0;
+            }
+          }
+        }
+
+        if (!hasMoved) {
           currentLoopIndex = 0;
         }
-        window.requestAnimationFrame(step);
+
+        drawFrame(
+          CYCLE_LOOP[currentLoopIndex],
+          currentDirection,
+          positionX,
+          positionY
+        );
+        window.requestAnimationFrame(gameLoop);
       }
-      function init() {
-        window.requestAnimationFrame(step);
+
+      function moveCharacter(deltaX, deltaY, direction) {
+        if (
+          positionX + deltaX > 0 &&
+          positionX + SCALED_WIDTH + deltaX < canvas.width
+        ) {
+          positionX += deltaX;
+        }
+        if (
+          positionY + deltaY > 0 &&
+          positionY + SCALED_HEIGHT + deltaY < canvas.height
+        ) {
+          positionY += deltaY;
+        }
+        currentDirection = direction;
       }
     },
   },
